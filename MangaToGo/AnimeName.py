aad3d5c4ab96d97999a08f14d8ea6438367d7dd5
Yelets <nelsonquiew@gmail.com>
2024-11-21 13:40:33 -0600

    Update AnimeName.py

--- MangaToGo/AnimeName.py
+++ MangaToGo/AnimeName.py
@@ -149,13 +149,10 @@ def main():
 	ids = get_chapter_id(mangaID)
 
 
-	#print(json.dumps(ids,indent=2))
-
-
 	filtered_data = [item for item in ids if item[4] == None]
 	sorted_data = sorted(filtered_data, key=lambda x: (
-		int(x[1]) if x[1] is not None else 0,
 		str(x[2]),
+		int(x[1]) if x[1] is not None else 0,
 		x[3] if x[3] is not None else '',
 	))
 
